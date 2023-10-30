936e5075ebf4d4ed64e662ad8eab7f974d23c8c6
jazzjazzy <jsjazzau@gmail.com>
2023-10-30 14:19:37 +1100

    added user instructions

--- index.php
+++ index.php
@@ -6,6 +6,19 @@ $postion = "0,0,NORTH";
 
 $robot = new Robot($postion);
 
+print "
+Instruction:\n
+Control the robot by using a set of commands to move it around a table in a 5 X 5 grid (0-4)
+
+To move the robot around the table use the commands PLACE, MOVE, LEFT, RIGHT, REPORT
+PLACE: will move the robot to a new position on the table, default Position is 0,0,NORTH
+MOVE: Will move one grid square in the direction it is facing, if its movement places off the table it will tell you it is in danger and not move
+LEFT: Will rotate the robot one compass setting to the left
+RIGHT: Will rotate the robot one compass setting to the right
+REPORT: Will output am message telling you the current position the robot is in and which way it is facing
+
+Enter you commands :\n";
+
 while(1){
     $input = trim(fgets(STDIN));
 
